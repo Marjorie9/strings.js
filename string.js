511212c0c7008e23f3617ceab81cce7718f21ce8
Marjorie9 <marjoriekatedeguzman@gmail.com>
2024-09-14 17:28:14 +0800

    Add files via upload

---
+++ string.js
@@ -0,0 +1,27 @@
+// Create a variable named facActorFirstName and store your favorite actor's first name
+ let favActorFirstName = "Robert";
+
+ // Create a variable named favActorLastName and store their last name
+ let favActorLastName = "Downey Jr.";
+
+ //Concatenate the first and last names into a variable named fullName
+ let fullName = Robert + " " Downey Jr.
+
+ // Create a variable named uppercase and store lowercase version of your favorite actor's name
+ let lowercase = 
+ fullName.toLowerCase();
+
+ //Create a variable named lowercasw and store the capitalized version of your favorite pet's name
+ let favPetName = "lizzy";
+let uppercase = favPetName.charAt(0).toUpperCase() + favPetName.slice(1);
+
+//Create a variable named message and store a message like:"My favorite actor is [ROBERT DOWNEY JR.]"
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+Append the message to include "his best show is Avengers."
+message += " His best show is Avengers. ";
+
+console.log(message);
+
+
+
